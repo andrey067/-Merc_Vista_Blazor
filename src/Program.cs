74d3af07b41bbb7c7441c3fffffbb8d2ce66760e
audrey.lima <audrey.lima@ms.senac.br>
2023-05-19 10:11:12 -0400

    Ajuste nas configurações do HttpClient

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,21 @@
+using Merc_Vista_Blazor;
+using Merc_Vista_Blazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Merc_Vista_Blazor;
 using MudBlazor.Services;
-using Merc_Vista_Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ServicesUrlOptions:ApiBaseUrl"]!) });
+builder.Services.AddScoped(sp =>
+{
+    var httpClient = new HttpClient();
+    var apiBaseUrl = builder.Configuration["ServicesUrlOptions:ApiBaseUrl"]!;
+    httpClient.DefaultRequestHeaders.Add("Origin", "*");
+    httpClient.BaseAddress = new Uri(apiBaseUrl);
+    return httpClient;
+});
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<IServiceCaller, ServiceCaller>();
